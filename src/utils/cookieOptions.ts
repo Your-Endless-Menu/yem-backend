99bcc96f2c0a2cfb5febb5ebe6de493cac08c52d
Pavlo Yevtushenko <45912881+stclamp@users.noreply.github.com>
2024-06-02 18:09:06 +0300

    Update cookieOptions.ts

--- src/utils/cookieOptions.ts
+++ src/utils/cookieOptions.ts
@@ -1,13 +1,13 @@
 export const httpOnlyCookieOptions = {
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: 'lax',
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
 };
 
 export const commonCookieOptions = {
     httpOnly: false,
-    secure: false,
+    secure: true,
     sameSite: 'lax',
     expires: new Date(Date.now() + 30 * 60 * 1000),
 };
